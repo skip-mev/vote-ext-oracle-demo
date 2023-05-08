ff48e7ea5f568014caeb7c96a57a0d9b6981aba3
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-05-08 16:08:31 -0400

    updates

--- abci/proposal.go
+++ abci/proposal.go
@@ -1,1 +1,20 @@
 package abci
+
+import (
+	abci "github.com/cometbft/cometbft/abci/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type ProposalHandler struct{}
+
+func (h *ProposalHandler) PrepareProposal() sdk.PrepareProposalHandler {
+	return func(ctx sdk.Context, req abci.RequestPrepareProposal) abci.ResponsePrepareProposal {
+		panic("not implemented")
+	}
+}
+
+func (h *ProposalHandler) ProcessProposal() sdk.ProcessProposalHandler {
+	return func(ctx sdk.Context, req abci.RequestProcessProposal) abci.ResponseProcessProposal {
+		panic("not implemented")
+	}
+}
